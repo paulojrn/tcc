c69511e2922c9f064f4dd289f2aee24c1a00630b
Paulo JRN <paulo.xcco@gmail.com>
2017-10-19 03:36:41 -0200

    Adicionando a aba Use case e um exemplo de funcionamento

--- app/Template/project_header/views.php
+++ app/Template/project_header/views.php
@@ -8,9 +8,6 @@
     <li <?= $this->app->checkMenuSelection('TaskListController') ?>>
         <?= $this->url->icon('list', t('List'), 'TaskListController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-listing', t('Keyboard shortcut: "%s"', 'v l')) ?>
     </li>
-    <li <?= $this->app->checkMenuSelection('UseCaseController') ?>>
-        <?= $this->url->icon('user-circle-o', t('Use case'), 'UseCaseController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false) ?>
-    </li>
 
     <?= $this->hook->render('template:project-header:view-switcher', array('project' => $project, 'filters' => $filters)) ?>
 </ul>
